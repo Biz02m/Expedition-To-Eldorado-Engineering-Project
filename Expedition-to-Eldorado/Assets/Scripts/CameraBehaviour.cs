9baf20e5216dfbd1a268841ae81f0bf75cc322a6
Mikołaj Bisewski <mikolajbisewski@wp.pl>
2024-03-03 17:30:30 +0100

    implemented mouse camera movement

--- Expedition-to-Eldorado/Assets/Scripts/CameraBehaviour.cs
+++ Expedition-to-Eldorado/Assets/Scripts/CameraBehaviour.cs
@@ -9,11 +9,13 @@ public class CameraBehaviour : MonoBehaviour
     [SerializeField] DeckManager deckManager;
     [SerializeField] int viewType = (int)ViewTypes.BoardCards; //default setting
     int lastViewType;
-    [SerializeField] float cameraHeight = 50;
-    [SerializeField] float cameraSpeed = 5;
+    [SerializeField] float cameraHeight = 50f;
+    [SerializeField] float cameraSpeed = 5f;
+    [SerializeField] float mouseCameraSpeed = 10f;
     Vector3[] camPositions = new Vector3[4];
     private Quaternion _targetRotation = Quaternion.identity;
     public float turningRate = 30f;
+    private float mouseBorder = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -34,19 +36,25 @@ public class CameraBehaviour : MonoBehaviour
         camPositions[(int)ViewTypes.Shop].y = cameraHeight;
         camPositions[(int)ViewTypes.Shop].z = 0;
 
-        setView();
+        setView(new Vector3(0,0));
     }
 
     // Update is called once per frame
     void Update()
     {
         handlePlayerInput();
+        moveCamera();
+    }
+
+    private void moveCamera()
+    {
         mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, camPositions[viewType], cameraSpeed * Time.deltaTime);
         mainCamera.transform.rotation = Quaternion.RotateTowards(mainCamera.transform.rotation, _targetRotation, turningRate * Time.deltaTime);
     }
 
     private void handlePlayerInput()
     {
+        //check for changed view mode
         bool viewIsChanged = false;
         if (Input.GetKeyDown(KeyCode.W))
         {
@@ -79,15 +87,44 @@ public class CameraBehaviour : MonoBehaviour
             viewIsChanged = true;
         }
 
+
+        //check if player moved the camera with mouse
+        Vector3 pos = transform.position;
+        if (Input.mousePosition.y >= Screen.height - mouseBorder)
+        {
+            pos.z += mouseCameraSpeed * Time.deltaTime;
+            viewIsChanged = true;
+        }
+
+        if(Input.mousePosition.y <= mouseBorder)
+        {
+            pos.z -= mouseCameraSpeed * Time.deltaTime;
+            viewIsChanged = true;
+        }
+
+        if (Input.mousePosition.x >= Screen.width - mouseBorder)
+        {
+            pos.x += mouseCameraSpeed * Time.deltaTime;
+            viewIsChanged = true;
+        }
+
+        if (Input.mousePosition.x <= mouseBorder)
+        {
+            pos.x -= mouseCameraSpeed * Time.deltaTime;
+            viewIsChanged = true;
+        }
+        transform.position = pos;
+
         if (viewIsChanged)
         {
-            setView();
+            setView(pos);
         }
     }
 
-    private void setView()
+    private void setView(Vector3 camPosition)
     {
         deckManager.changeView((ViewTypes)viewType);
+        //hardcoded values
         switch (viewType)
         {
             case (int)ViewTypes.CardsOnly:
@@ -103,9 +140,15 @@ public class CameraBehaviour : MonoBehaviour
                 SetBlendedEulerAngles(new Vector3(60, -30, 2));
                 break;
         }
-        Debug.Log(this.viewType);
+
+        for(int i = 0; i < (int)ViewTypes.NumOfViewTypes && i != (int)ViewTypes.Shop; i++)
+        {
+            camPositions[i].x = camPosition.x;
+            camPositions[i].z = camPosition.z;
+        }
     }
 
+    //i have no idea what this means
     public void SetBlendedEulerAngles(Vector3 angles)
     {
         _targetRotation = Quaternion.Euler(angles);
